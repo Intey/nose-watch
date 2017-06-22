16dd1c1190795ca7c83d784729bc68804953b338
Churin Dmitry <ziexe0@gmail.com>
2017-06-22 13:12:19 +0300

    [fix] Xplatform current directory

--- nosewatch/plugin.py
+++ nosewatch/plugin.py
@@ -1,4 +1,6 @@
 import sys
+import os
+
 from nose.plugins import Plugin
 from subprocess import Popen
 
@@ -18,10 +20,11 @@ class WatchPlugin(Plugin):
         argv = list(self.sys.argv)
         argv.remove('--with-watch')
         watchcmd = 'clear && ' + ' '.join(argv)
-        call_args = ['watchmedo', 'shell-command', '-c',
-            watchcmd, '-R', '-p', '*.py', '-W', '.']
+        call_args = ['watchmedo', 'shell-command', 
+                     '-c', watchcmd, '-R', 
+                     '-p', '*.py',
+                     '-W', os.path.abspath(os.path.curdir)]
         try:
             self.call(call_args)
         except KeyboardInterrupt:
             self.sys.stdout.write('\nStopped\n')
-
